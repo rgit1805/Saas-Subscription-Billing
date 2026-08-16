9811ed04dfaee56d033e69df5c1b7dd672d1a2a6
RICHA PANDEY <richapandey03.vaar@gmail.com>
2026-08-17 01:14:39 +0530

    Commit

--- config/settings.py
+++ config/settings.py
@@ -81,11 +81,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "saas_billing",
-        "USER": "postgres",
-        "PASSWORD": "YOUR_POSTGRES_PASSWORD",
-        "HOST": "localhost",
-        "PORT": "5432",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
